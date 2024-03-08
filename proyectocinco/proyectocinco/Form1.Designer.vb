8580fe61292ffcf0514b2a34cd5bb0230dbb3bce
= <=>
2024-03-07 20:57:32 -0600

    Primera subida

---
+++ proyectocinco/proyectocinco/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.listBoxTablas = New System.Windows.Forms.ListBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.25!)
+        Me.Label1.Location = New System.Drawing.Point(41, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(267, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Calcular la tabla del nueve"
+        '
+        'listBoxTablas
+        '
+        Me.listBoxTablas.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.listBoxTablas.FormattingEnabled = True
+        Me.listBoxTablas.ItemHeight = 18
+        Me.listBoxTablas.Location = New System.Drawing.Point(46, 48)
+        Me.listBoxTablas.Name = "listBoxTablas"
+        Me.listBoxTablas.Size = New System.Drawing.Size(199, 274)
+        Me.listBoxTablas.TabIndex = 1
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(283, 83)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 2
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(400, 360)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.listBoxTablas)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents listBoxTablas As ListBox
+    Friend WithEvents btnCalcular As Button
+End Class
---
+++ proyectocinco/proyectocinco/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim res As Integer
+
+        listBoxTablas.Items.Clear()
+
+        For i As Integer = 1 To 10
+            res = (i * 9)
+            listBoxTablas.Items.Add("9x" & i & "=" & res)
+        Next
+
+
+    End Sub
+End Class
